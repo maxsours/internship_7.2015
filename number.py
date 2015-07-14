a1342e685fda33f0a8eb27d5812ce8fd5e234755
maxsours <maxsours@gmail.com>
2015-07-14 09:46:12 -0400

    Added number.py

--- number.py
+++ number.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World"
-
+@app.route('/<int:number>')
+def square(number):
+    return number ** 2
+    
 if __name__ == "__main__":
     app.run()
